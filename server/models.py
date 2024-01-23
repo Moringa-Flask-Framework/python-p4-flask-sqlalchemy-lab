fbe95874cebc9cf77bb848e1ed6d8eb2fa865746
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-23 14:39:05 +0300

    Table migrations

--- server/models.py
+++ server/models.py
@@ -11,13 +11,26 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    birthday= db.Column(db.String)
+
+    animals= db.relationship('Animal', backref= 'zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment= db.Column(db.String)
+    open_to_visitors= db.Column(db.Boolean)
+
+    animals = db.relationship("Animal", uselist=False, backref= 'enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    species = db.Column(db.String)
+    enclosure_id = db.Column(db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.ForeignKey('zookeepers.id'))
+
